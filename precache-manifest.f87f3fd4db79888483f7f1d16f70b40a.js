86b0e880e9322074dea8d5e0a743899ca5f0444f
vkaszuba <codeword.vika@gmail.com>
2020-04-09 19:25:35 -0700

    Updates

---
+++ precache-manifest.f87f3fd4db79888483f7f1d16f70b40a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc3b4bcef9b0d9ea3eeef3e1826f49f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ee04cd42bad54f14cc6",
+    "url": "/static/js/2.1f15292e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1f15292e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c4a631255fbc8bbf779",
+    "url": "/static/js/main.7d2293bc.chunk.js"
+  },
+  {
+    "revision": "cc9f0b8c7d9020a60ad1",
+    "url": "/static/js/runtime-main.e3cbaa3d.js"
+  }
+]);
