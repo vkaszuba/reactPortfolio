377c1075fe056231d0c75cb55b87fd404dff3080
vkaszuba <codeword.vika@gmail.com>
2020-04-10 16:48:53 -0700

    Change to small screen nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,28 +3,29 @@ import React from 'react';
 const Header = props => {
   return (
     <header>
-      <div className="col">
+      <div className="row">
         <div className="nameBox">
           <h1>VICTORIA KASZUBA</h1>
         </div>
       </div>
 
-      <div className="col">
-          <div className="dropdown">
-            <button className="btn shadow-none" type="button" data-toggle="dropdown">
-              <div className="menuFont">X</div>
-            </button>
-            <div className="dropdown-menu">
-              <a href="index.html" className="linkFont">
-                <h6>//</h6> About
-              </a>
-              
-              <a href="portfolio.html" className="linkFont">// Portfolio</a>
-              <a href="resume.html" className="linkFont">// Resume</a>
-              <a href="contact.html" className="linkFont">// Contact</a>
-            </div>
-          </div>
+      <div className="row">
+        <div className="navSmall">
+          <a href="#about"
+            onClick={() => props.handlePageChange("About")}
+            className="linkFont"
+            link={props.currentPage === 'About' ? "nav-link active" : 'nav-link'}>
+            About</a>
+//
+              <a href="#portfolio"
+            onClick={() => props.handlePageChange("Portfolio")}
+            className="linkFont"
+            link={props.currentPage === 'Portfolio' ? "nav-link active" : 'nav-link'}>Portfolio</a>
+
         </div>
+      </div>
+
+
     </header>
   )
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,18 +5,20 @@ const Navbar = props => {
   return (
 
     <div className="col-xs-0 col-md-2">
-    <div className="navBar">
-    <a href="#about" 
-           onClick={() => props.handlePageChange("About")} 
-           className="linkFont"
-           link={ props.currentPage === 'About' ? "nav-link active" : 'nav-link'}>About</a>
-
-        <a href="#portfolio" 
-           onClick={() => props.handlePageChange("Portfolio")} 
-           className="linkFont"
-           link={ props.currentPage === 'Portfolio' ? "nav-link active" : 'nav-link'}>Portfolio</a>
-
-    </div>
+      <div className="navBar">
+        <div className="row">
+          <a href="#about"
+            onClick={() => props.handlePageChange("About")}
+            className="linkFont"
+            link={props.currentPage === 'About' ? "nav-link active" : 'nav-link'}>About</a>
+        </div>
+        <div className="row">
+          <a href="#portfolio"
+            onClick={() => props.handlePageChange("Portfolio")}
+            className="linkFont"
+            link={props.currentPage === 'Portfolio' ? "nav-link active" : 'nav-link'}>Portfolio</a>
+        </div>
+      </div>
     </div>
   )
 };
