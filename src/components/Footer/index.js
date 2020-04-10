f8cfa33d3fbb693a241779a10d5680a65d4be0aa
vkaszuba <codeword.vika@gmail.com>
2020-04-09 20:14:55 -0700

    Added footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,25 @@
+
+
+
+import React from 'react';
+
+const Footer = props => {
+  return (
+
+    <div class="row">
+      <div class="col">
+        <footer>
+          <a href="https://www.linkedin.com/in/victoriakaszuba/" target="_blank">
+            <img class="socialMediaImage" alt="LinkedIn" src="../../images/logo-linkedin-white.png"/>
+          </a>
+          <a href="https://github.com/vkaszuba" target="_blank">
+            <img class="socialMediaImage" alt="GitHub" src="../../images/logo-github-white.png"/>
+          </a>
+        </footer>
+      </div>
+    </div>
+  
+    )
+      };
+
+  export default Footer
--- src/containers/App/app.js
+++ src/containers/App/app.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './../../components/Header'
 import Navbar from './../../components/Navbar'
+import Footer from './../../components/Footer'
 
 class App extends Component {
 
@@ -10,6 +11,7 @@ class App extends Component {
 
         <Header />
         <Navbar />
+        <Footer/>
 
       </div>
     );
