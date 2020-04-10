b0f1bdbdab300aa27187eb777e51054cbd452683
vkaszuba <codeword.vika@gmail.com>
2020-04-09 20:31:15 -0700

    Adding small screen navbar version

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,14 +6,14 @@ import React from 'react';
 const Footer = props => {
   return (
 
-    <div class="row">
-      <div class="col">
+    <div className="row">
+      <div className="col">
         <footer>
           <a href="https://www.linkedin.com/in/victoriakaszuba/" target="_blank">
-            <img class="socialMediaImage" alt="LinkedIn" src="../../images/logo-linkedin-white.png"/>
+            <img className="socialMediaImage" alt="LinkedIn" src="../../images/logo-linkedin-white.png"/>
           </a>
           <a href="https://github.com/vkaszuba" target="_blank">
-            <img class="socialMediaImage" alt="GitHub" src="../../images/logo-github-white.png"/>
+            <img className="socialMediaImage" alt="GitHub" src="../../images/logo-github-white.png"/>
           </a>
         </footer>
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,31 @@ import React from 'react';
 
 const Header = props => {
   return (
-        <div class="col">
-        <div class="nameBox">
+    <header>
+      <div className="col">
+        <div className="nameBox">
           <h1>VICTORIA KASZUBA</h1>
         </div>
       </div>
-  )
-      };
 
-      export default Header;
+      <div className="col">
+          <div className="dropdown">
+            <button className="btn shadow-none" type="button" data-toggle="dropdown">
+              <div className="menuFont">X</div>
+            </button>
+            <div className="dropdown-menu">
+              <a href="index.html" className="linkFont">
+                <h6>//</h6> About
+              </a>
+              
+              <a href="portfolio.html" className="linkFont">// Portfolio</a>
+              <a href="resume.html" className="linkFont">// Resume</a>
+              <a href="contact.html" className="linkFont">// Contact</a>
+            </div>
+          </div>
+        </div>
+    </header>
+  )
+};
+
+export default Header;
